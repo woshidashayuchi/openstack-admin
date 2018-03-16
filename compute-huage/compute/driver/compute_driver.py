dc8793296f7330996ff80e9d53dbaeb35c2bfcd0
woshidashayuchi <359876749@qq.com>
2018-03-16 11:52:42 +0800

    compute

--- compute-huage/compute/driver/compute_driver.py
+++ compute-huage/compute/driver/compute_driver.py
@@ -1,9 +1,201 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: YanHua <yunshu360@163.com>
+# Author: wxf<wangxiaofeng1@hualala.com>
+# Time: 2018/3/15 11:01
+import sys
+p_path = sys.path[0] + '/..'
+p_path1 = sys.path[0] + '/../..'
+sys.path.insert(1, p_path)
+sys.path.insert(1, p_path1)
+from common.parameters import rpc_data, context_data
+from common.logs import logging as log
+from common.code import request_result
+from common.rabbitmq_client import RabbitmqClient
 
 
 class ComputeDriver(object):
-
     def __init__(self):
-        pass
+        self.rbtmq = RabbitmqClient()
+        self.queue = "cinder_api"
+        self.timeout = 5
+
+    # 同restapi_define的context、parameters一致
+    def create_osdisk(self, context, parameters):
+        '''
+        :param context: dict
+        :param parameters: dict: {size=int,
+                                  name=string,
+                                  v_type=string,
+                                  description=string,
+                                  snapshot_uuid=string,
+                                  source_volume_uuid=string
+                                  image_uuid=string}
+        :return:
+        '''
+        try:
+            rpc_body = rpc_data("osdisk_cre", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def clouddisk_create(self, context, parameters=None):
+        '''
+        :param context: {'token':string,
+                         'source_ip':source_ip}
+        :param parameters: {size=int,
+                            name=string,
+                            v_type=string,
+                            description=string,
+                            snapshot_uuid=string,
+                            source_volume_uuid=string}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               'vol_vol_pro_com',
+                               'create',
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("clouddisk_cre", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def clouddisk_delete(self, context, parameters):
+
+        '''
+
+        :param context:
+        :param parameters: {"volume_uuid": string,
+                            "logic": 1(逻辑删除)/0（物理删除）}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               parameters.get('volume_uuid'),
+                               'delete',
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("clouddisk_del", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def clouddisk_info(self, context, parameters={}):
+        '''
+
+        :param context:
+        :param parameters:{'page_size':int,
+                           'page_num': int}
+                            可选
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               'vol_vol_usr_com',
+                               'read')
+        try:
+            rpc_body = rpc_data("clouddisk_inf", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def clouddisk_recovery(self, context, parameters):
+        '''
+
+        :param context: {"token":string,
+                         "source_ip"}
+        :param parameters: {"volume_uuid": string}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               parameters.get('volume_uuid'),
+                               'update',
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("clouddisk_rec", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def disk_snapshot_delete(self, context, parameters):
+        '''
+
+        :param context: {"token":string,
+                          "source_ip": string}
+        :param parameters:{"snapshot_uuid":string,
+                           "logic":1/0}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               parameters.get('snapshot_uuid'),
+                               "delete",
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("snap_del", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def disk_snapshot_revert(self, context, parameters):
+        '''
+
+        :param context:
+        :param parameters: {"snapshot_uuid":string}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               parameters.get('snapshot_uuid'),
+                               'update',
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("snap_rev", context, parameters)
+
+            return self.rbtmq.rpc_cast_client(self.queue,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
+
+    def disk_snapshot_revert_wait(self, context, parameters):
+        '''
+
+        :param context:{'token':string,
+                        'source_ip':string,
+                        }
+        :param parameters: {"snapshot_uuid":string}
+        :return:
+        '''
+        context = context_data(context.get('token'),
+                               parameters.get('snapshot_uuid'),
+                               'update',
+                               context.get('source_ip'))
+        try:
+            rpc_body = rpc_data("snap_rev_wait", context, parameters)
+
+            return self.rbtmq.rpc_call_client(self.queue,
+                                              self.timeout,
+                                              rpc_body)
+        except Exception, e:
+            log.error('Rpc client exec error, reason=%s' % e)
+            return request_result(598)
--- compute-huage/compute/driver/openstack_driver.py
+++ compute-huage/compute/driver/openstack_driver.py
@@ -1,71 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: YanHua <yunshu360@163.com>
-import os
 import sys
 sys.path.append(sys.path[0] + '/..')
 sys.path.append(sys.path[0] + '/../..')
-
-from common.logs import logging as log
-from openstacksdk.openstack import profile
-from openstacksdk.openstack import connection
 from common.logs import logging as log
 from common.code import request_result
+from conf import conf
+import openstack
+
+def connection():
+    try:
+        conn = openstack.connect(cloud=conf.conn_cloud)
+    except Exception, e:
+        log.error('connect to op error, reason is: %s' % e)
+        return False
+
+    return conn
 
 class OpenstackDriver(object):
 
-    def __init__(self, auth_url, region, project_name,
-                 username, password):
+    def __init__(self):
 
-        prof = profile.Profile()
-        prof.set_region(profile.Profile.ALL, region)
+        self.conn = connection()
 
-        self.conn = connection.Connection(
-                               profile=prof,
-                               user_agent='examples',
-                               auth_url=auth_url,
-                               project_name=project_name,
-                               username=username,
-                               password=password)
-
-    # def create_server(self, image_name,
-    #                   flavor_name, network_name):
-
-        # image = self.conn.compute.find_image(IMAGE_NAME)
-        # flavor = self.conn.compute.find_flavor(FLAVOR_NAME)
-        # network = self.conn.network.find_network(NETWORK_NAME)
-        # keypair = create_keypair(self.conn)
-        #
-        # server = self.conn.compute.create_server(
-        #               name=SERVER_NAME, image_id=image.id, flavor_id=flavor.id,
-        #               networks=[{"uuid": network.id}], key_name=keypair.name)
-        #
-        # result = self.conn.compute.wait_for_server(server)
-        #
-        # return result
-
-    def create(self, availzone_uuid, image_uuid, vm_name, nic_list,
-               security_groups=None, keypair=None, flavor_id='2'):
+    def create_server(self, availzone_uuid, image_uuid, vm_name, nic_list,
+                      security_groups=None, keypair=None):
         '''
-
         :param availzone_uuid: 目前亲测方法用的是可用域的名称，id暂不知可否使用
         :param image_uuid: 镜像id
         :param vm_name: 实例名称
         :param nic_list: 网络列表。[{'uuid':'net01-uuid'},{'uuid': 'net02-uuid'}]
         :param security_groups [{'name':''},{'name':''}]可选
         :param key_name 可选
-        :flavor_id: 规格
         :return: .result为新建实例的object
         '''
         try:
             op_result = self.conn.\
                 compute.create_server(name=vm_name,
                                       availability_zone=availzone_uuid,
-                                      image_id=image_uuid,  # id
+                                      image_id=image_uuid,
                                       # security_groups=security_groups,
                                       networks=nic_list,
                                       # key_name=keypair,
-                                      flavor_id=flavor_id  # str
+                                      flavor_id="2"  # str
                                       )
         except Exception, e:
             log.error('create the cloudhost(op) error, reason is: %s' % e)
@@ -73,7 +51,7 @@ class OpenstackDriver(object):
 
         return request_result(0, op_result)
 
-    def delete(self, vm_uuid):
+    def delete_server(self, vm_uuid):
         '''
         :param cloudhost_uuid: 实例id
         :return: .result是实例id
@@ -125,15 +103,43 @@ class OpenstackDriver(object):
             log.error('remove the floating ip from server(op) error, '
                       'reason is: %s' % e)
             return request_result(632)
-
+        log.debug('unbind floatip result(op) is: %s' % op_result)
         return request_result(0, 'removed')
+
+    def vm_pwd_reset(self, token, server_uuid, new_password):
+        try:
+            op_result = self.conn.compute.\
+                change_server_password(server_uuid, new_password)
+        except Exception, e:
+            log.error('reset the password of the vm error, reason is: %s' % e)
+            return request_result(641)
+
+        log.info('pwd reset the result(op) is %s' % op_result) # None
+        return request_result(0, {'server_uuid': server_uuid})
 
 
 # test-code
 if __name__ == '__main__':
-    c = OpenstackDriver()
-    a = c.create(availzone_uuid='nova',
-                 image_uuid='aafad1da-b3e8-4384-ae6a-93829cde4d33',
-                 vm_name='test',
-                 nic_list=[{'uuid': '65989d42-8827-44c7-a1ed-838321e4941a'}])
-    print a
+    op = OpenstackDriver()
+    # 创建vm
+    print op.\
+        create_server(availzone_uuid='nova',
+                      image_uuid='aafad1da-b3e8-4384-ae6a-93829cde4d33',
+                      vm_name='test111',
+                      nic_list=[{'uuid': '65989d42-8827-44c7-a1ed-838321e4941a'}])
+
+    # 删除vm
+    # ret = op.delete_server(vm_uuid='040497a3-7a31-42ad-8d6d-630906a679f2')
+
+    # 修改vm密码
+    # op.vm_pwd_reset(token='',
+    #                 server_uuid='8db8185a-68d7-4643-ad93-5520bec2ffd4',
+    #                 new_password='qwe123')
+
+    # op.vm_boot('', '19a1f791-4c02-4fa9-8d0c-d9f8ad566f55')
+    #
+    # op.vm_shut('', '19a1f791-4c02-4fa9-8d0c-d9f8ad566f55')
+    #
+    # op.floatip_bind('', vm_uuid='19a1f791-4c02-4fa9-8d0c-d9f8ad566f55', floatip='172.20.2.8')
+    #
+    # op.floatip_unbind('', vm_uuid='19a1f791-4c02-4fa9-8d0c-d9f8ad566f55', floatip='172.20.2.8')
